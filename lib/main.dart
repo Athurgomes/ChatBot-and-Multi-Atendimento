f66272b40411a9f72acaa9f717f5d78d7ad77988
Athurgomes <arthurgomesoliveira004@gmail.com>
2025-01-30 20:19:38 -0300

    Apenas adicionei um ignore na main e removi a superkey do login, devido a não necessidade

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:chatbot_multiatendimento/Screens/registration_screen/cadastroUser.dart';
+// ignore: unused_import
+import 'package:chatbot_multiatendimento/Screens/login_screen/login.dart';
+
 
 void main() {
   runApp(const MyApp());
